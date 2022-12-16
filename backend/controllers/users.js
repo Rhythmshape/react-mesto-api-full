bd545903d4638582fa99be2aca19ef04d4773dba
Alena Khalina <rhythmshape@yandex.ru>
2022-12-16 17:42:56 +0500

    Проектная работа 15

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -115,7 +115,7 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        `${NODE_ENV === 'production' ? JWT_SECRET : 'yandex-praktikum'}`,
+        `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`,
         { expiresIn: '7d' },
       );
       res.send({ token, message: 'Успешная Авторизация!' });
